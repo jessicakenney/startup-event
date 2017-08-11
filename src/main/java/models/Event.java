4030d90f1be17a0f5e4fa17fefdffba2ddcebbf3
jessicakenney <jessicakenney@yahoo.com>
2017-08-11 10:10:08 -0700

    successful instantiation of Event class

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,4 +1,15 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Event {
+  private String name;
+  private String description;
+  private ArrayList<String> attendees;
+
+  public Event(String name, String description, ArrayList<String>attendees) {
+
+  }
+
 }
+
--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -1,6 +1,10 @@
 package models;
 
 import org.junit.After;
+import org.junit.Test;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -10,6 +14,18 @@ public class EventTest {
   public void tearDown() throws Exception {
     //Event.clearAllPosts(); //clear out allll the posts before each test.
   }
+  //helper
+  public Event newEvent() {
+    ArrayList<String> names = new ArrayList<String>(Arrays.asList("name1","name2"));
+    return new Event("Event1", "description1", names);
+  }
+
+  @Test
+  public void newEvent_InstantiatesCorrectly_true() throws Exception {
+    Event testEvent = newEvent();
+    assertEquals(true, testEvent instanceof Event);
+  }
+
 
 
 
